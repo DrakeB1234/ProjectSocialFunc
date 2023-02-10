78baed0185d76f2583c0735cbebbd58c0f59657d
drakebuentello <DrakeB1234>
2023-02-10 16:30:36 -0600

    intialized project with create-next-app, go rid of default styles, added custom favicon and banners

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SocialFunc | Homepage</title>
+      </Head>
+      <main>
+        <img src='/appbanner-socialfunc.png' width='900px'/>
+        <h1>SocialFunc Homepage</h1>
+      </main>
+    </>
+  )
+}
