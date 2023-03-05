19628d23e12dfee1d3ef02538dbba69e422b051c
drakebuentello <DrakeB1234>
2023-03-04 21:51:27 -0600

    fixed issue with API not connecting to DB

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.SUPABASE_URL,
+    CONTENTFUL_ACCESS_TOKEN: process.env.SUPABASE_KEY,
+  },
 }
 
 module.exports = nextConfig
